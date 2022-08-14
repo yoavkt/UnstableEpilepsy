a94ddd00533abd56e9e7405c3fc16ab3e4a603ee
Yoav Kan-Tor <Yoav.Kan-Tor@ibm.com>
2022-08-14 17:45:02 +0300

    added imputer

--- epilepsy_prediction/epilepsy_prediction/__init__.py
+++ epilepsy_prediction/epilepsy_prediction/__init__.py
@@ -3,8 +3,9 @@ __version__ = "0.1.0"
 __author__ = 'Yoav Kan-Tor'
 __credits__ = 'IBM research'
 from .utils import data_preprocess, load_model
-
+from .imputation import missing_imputer
 __all__ = [
     "data_preprocess",
     "load_model",
+    "missing_imputer"
 ]
--- epilepsy_prediction/epilepsy_prediction/imputation.py
+++ epilepsy_prediction/epilepsy_prediction/imputation.py
@@ -1,8 +1,54 @@
 
+from copy import deepcopy
+from tabnanny import verbose
+from typing_extensions import Self
 from sklearn.impute._base import _BaseImputer
-
-class missing_imputer(_BaseImputer):
+from sklearn.linear_model import LassoCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+class missing_imputer:
     """
 
     """
-    def __init__(self):
+
+    def __init__(self,numeric_estimator=None,binary_estimator=None,missing_cols=None):
+        if numeric_estimator is None:
+            numeric_estimator = LassoCV()
+        self._base_numeric_estimator = numeric_estimator
+        if binary_estimator is None:
+            binary_estimator = GridSearchCV(LogisticRegression(penalty='elasticnet',solver='saga'),
+                                            param_grid={'l1_ratio':np.arange(0,1,0.1)})
+        self._base_binary_estimator = binary_estimator
+        self.missing_cols = missing_cols
+        self.estimator_dict = {}
+        return None
+
+    def _get_data_set(self,X,target,isnull=False):
+        train_X = X.loc[X[target].isnull()==isnull,X.columns.isin(self.missing_cols)]
+        train_y = X.loc[X[target].isnull()==isnull,target]
+        return train_X,train_y
+    
+    def _get_estimator(self,X,target):
+        if X[target].dtype == 'bool':
+            return deepcopy(self._base_binary_estimator)
+        else:
+            return deepcopy(self._base_numeric_estimator)
+
+    def fit(self,X):
+        for col in self.missing_cols:
+            est = self._get_estimator(X,col)
+            X,y = self._get_data_set(X,col)
+            est.fit(X,y)
+            self.estimator_dict[col] = est
+        return Self
+
+    def transform(self,X):
+        for col in self.missing_cols:
+            X_pred,_ = self._get_data_set(X,isnull=True)
+            X.loc[X[col].isnull(),col] = self.estimator_dict[col].predict(X_pred)
+        return X
+
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.fit_transform(X)
